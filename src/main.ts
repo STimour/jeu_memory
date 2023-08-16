ee6a8e2dce15cf0aa6cae6b65c688bb8ac9cbdad
STimour <st.sayfoutdinov@icloud.com>
2023-08-16 16:52:34 +0200

    modification de main.ts et ajustement de la function

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "b
 
 const mainDiv = document.createElement('div') as HTMLDivElement;
     mainDiv.setAttribute("id", "mainDiv")
-    mainDiv.style.width = "80%"
+    mainDiv.style.width = "50%"
     mainDiv.style.margin = "auto"
     mainDiv.style.display = "flex"
     mainDiv.style.backgroundColor = "gray"
@@ -14,6 +14,9 @@ btnStart.addEventListener("click", () => {
 const container = document.querySelector('#container') as HTMLDivElement;
     container.style.margin = "0 0 20px 0";
 
+const card_front = document.createElement('div') as HTMLDivElement;
+    card_front.setAttribute("class", "card_front");
+
 
 
 let nb_jeu = 0;
@@ -24,35 +27,49 @@ function init() {
     container.innerHTML = `
         <button id="init-button">Nouveau Click</button>
         <p>nombre de tour:${nb_jeu}</p>
-        <div id="mainDiv"></div> 
+     
     `;
     
     container.appendChild(mainDiv);
-
     // Create new tiles for this round
     const tiles = new Array(16).fill('').map((_, i) => {
         const tile = document.createElement("div");
         tile.setAttribute("class", "tile");
         tile.style.width = "50px";
         tile.style.height = "50px";
+        tile.style.border = "1px solid black";
         tile.style.margin = "20px";
-        tile.style.backgroundColor = colors[Math.floor(i / 2)];
+        tile.style.backgroundColor = "black";
         return tile;
     });
-
     // Shuffle the new tiles
     tiles.sort(() => Math.random() - 0.5);
     // Clear previous tiles in mainDiv
     mainDiv.innerHTML = '';
-
     // Add the new tiles to mainDiv
     tiles.forEach(tile => mainDiv.appendChild(tile));
-
-    
-
     // Add an event listener
     const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
     newBtnStart.addEventListener("click", () => {
         init();
     });
+
+
+    let nodeList = document.querySelectorAll(".tile");
+    let elements = Array.from(nodeList);
+    elements.forEach( (element, i) => {
+        element.setAttribute("class", "not-revealed")
+        element.addEventListener("click", () => {
+            console.log(element.style.backgroundColor)
+            if (element.classList.contains("not-revealed")) {
+                element.style.backgroundColor = "black"; // Change background color to black
+                element.classList.remove("not-revealed");
+                element.classList.add("revealed");
+            } else {
+                element.style.backgroundColor = colors[Math.floor(i / 2)]; // Change background color back to original color
+                element.classList.remove("revealed");
+                element.classList.add("not-revealed");
+            }
+        })
+    })
 }
