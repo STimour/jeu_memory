2e0e3dc9412b0eab267fb9a5204dd84d7c4838d9
STimour <st.sayfoutdinov@icloud.com>
2023-08-16 15:26:01 +0200

    modification de main.ts et ajustement de la fonction init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,58 @@
 const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
-const mainDiv = document.createElement('mainDiv') as HTMLDivElement;
+const mainDiv = document.createElement('div') as HTMLDivElement;
+    mainDiv.setAttribute("id", "mainDiv")
+    mainDiv.style.width = "80%"
+    mainDiv.style.margin = "auto"
+    mainDiv.style.display = "flex"
+    mainDiv.style.backgroundColor = "gray"
+    mainDiv.style.border = "1px solid black"
 const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
-
 btnStart.addEventListener("click", () => {
     init()
 });
-nb_jeu = 0;
+const container = document.querySelector('#container') as HTMLDivElement;
+    container.style.margin = "0 0 20px 0";
 
 
-function init(){
+
+let nb_jeu = 0;
+function init() {
     console.log('init');
     btnStart.remove();
+    nb_jeu++;
+    container.innerHTML = `
+        <button id="init-button">Nouveau Click</button>
+        <p>nombre de tour:${nb_jeu}</p>
+        <div id="mainDiv"></div> 
+    `;
     
+    container.appendChild(mainDiv);
+
+    // Create new tiles for this round
+    const tiles = new Array(16).fill('').map((_, i) => {
+        const tile = document.createElement("div");
+        tile.setAttribute("class", "tile");
+        tile.style.width = "50px";
+        tile.style.height = "50px";
+        tile.style.margin = "20px";
+        tile.style.backgroundColor = colors[Math.floor(i / 2)];
+        return tile;
+    });
+
+    // Shuffle the new tiles
+    tiles.sort(() => Math.random() - 0.5);
+    // Clear previous tiles in mainDiv
+    mainDiv.innerHTML = '';
+
+    // Add the new tiles to mainDiv
+    tiles.forEach(tile => mainDiv.appendChild(tile));
+
+    
+
+    // Add an event listener
+    const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
+    newBtnStart.addEventListener("click", () => {
+        init();
+    });
 }
