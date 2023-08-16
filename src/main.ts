532c5b9604dac0a77e11fb7ff502727279e96cf8
STimour <st.sayfoutdinov@icloud.com>
2023-08-16 11:31:33 +0200

    integration du code source

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Attention aux "as" qui règlent pas mal de soucis
+const app = document.querySelector('#app') as HTMLDivElement;
+const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let cpt = 0;
+
+// Promises
+fetch("https://dog.ceo/api/breeds/image/random")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    document.querySelector("#image-chat")?.setAttribute("src", data.message);
+})
+
+// Add an event listener
+btnStart.addEventListener("click", () => {
+    init();
+});
+
+function init(){
+    console.log('init')
+    btnStart.remove();
+    cpt++;
+    app.innerHTML = `
+        <p>${cpt}</p>
+        <button id="init-button">Nouveau Click</button>
+    `;
+
+    const tiles = new Array(16).fill('').map( (_, i) => {
+        const tile = document.createElement("div")
+        tile.setAttribute("class", "tile")
+        tile.style.width ="50px"
+        tile.style.height = "50px"
+        tile.style.backgroundColor = colors[Math.floor(i/2)]
+        return tile
+    })
+    // Shuffle the tiles
+    tiles.sort( () => Math.random() - 0.5)
+    
+    // Add the tiles to the app
+    tiles.forEach( tile => app.appendChild(tile))
+
+    let nodeList = document.querySelectorAll(".tile");
+    let elements = Array.from(nodeList);
+    elements.forEach( (element) => {
+        element.setAttribute("class", "not-revvealed")
+        element.addEventListener("click", () => {
+            element.setAttribute("class", "revealed")
+        })
+    })
+    
+    // Add an event listener
+    const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
+    newBtnStart.addEventListener("click", () => {
+        init();
+    });
+}
