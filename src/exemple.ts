1206beafc571af64a7c1b9df818ffb56d72439b8
STimour <st.sayfoutdinov@icloud.com>
2023-08-17 17:03:53 +0200

    fonctionnement du jeu sans comptage des coups et de message de victoire

--- src/exemple.ts
+++ src/exemple.ts
@@ -1,62 +1,33 @@
-const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
+let nodeList = document.querySelectorAll(".tile"); // Получаем все элементы с классом "tile"
+let elements = Array.from(nodeList); // Преобразуем коллекцию в массив
 
-// Attention aux "as" qui règlent pas mal de soucis
-const app = document.querySelector('#app') as HTMLDivElement;
-const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
+let firstClickedTile = null; // Переменная для хранения первой нажатой плитки
 
-let cpt = 0;
+elements.forEach((element, i) => {
+    element.addEventListener("click", () => { // Добавляем обработчик клика для каждой плитки
+        if (element.classList.contains("not-revealed")) { // Если плитка не открыта
+            element.classList.remove("not-revealed");
+            element.classList.add("revealed"); // Показываем плитку
 
-// Promises
-fetch("https://dog.ceo/api/breeds/image/random")
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    document.querySelector("#image-chat")?.setAttribute("src", data.message);
-})
-
-// Add an event listener
-btnStart.addEventListener("click", () => {
-    init();
-});
-
-function init(){
-    console.log('init')
-    btnStart.remove();
-    cpt++;
-    app.innerHTML = `
-        <p>${cpt}</p>
-        <button id="init-button">Nouveau Click</button>
-    `;
-
-    const tiles = new Array(16).fill('').map( (_, i) => {
-        const tile = document.createElement("div")
-        tile.setAttribute("class", "tile")
-        tile.style.width ="50px"
-        tile.style.height = "50px"
-        tile.style.margin = "20px"
-        tile.style.backgroundColor = colors[Math.floor(i/2)]
-        return tile
-    })
-    // Shuffle the tiles
-    tiles.sort( () => Math.random() - 0.5)
-    
-    // Add the tiles to the app
-    tiles.forEach( tile => app.appendChild(tile))
-
-    let nodeList = document.querySelectorAll(".tile");
-    let elements = Array.from(nodeList);
-    elements.forEach( (element) => {
-        element.setAttribute("class", "not-revvealed")
-        element.addEventListener("click", () => {
-            element.setAttribute("class", "revealed")
-        })
-    })
-    
-    // Add an event listener
-    const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
-    newBtnStart.addEventListener("click", () => {
-        init();
+            if (!firstClickedTile) { // Если еще нет первой нажатой плитки
+                firstClickedTile = element; // Запоминаем эту плитку
+            } else {
+                if (firstClickedTile.style.backgroundColor === element.style.backgroundColor) { // Если цвета совпадают
+                    firstClickedTile = null; // Сбрасываем первую нажатую плитку для следующей пары
+                } else {
+                    // Если цвета не совпадают, переворачиваем обе плитки обратно
+                    setTimeout(() => {
+                        firstClickedTile.classList.remove("revealed");
+                        firstClickedTile.classList.add("not-revealed");
+                        element.classList.remove("revealed");
+                        element.classList.add("not-revealed");
+                        firstClickedTile = null;
+                    }, 1000); // Даем 1 секунду задержки перед переворотом
+                }
+            }
+        } else { // Если плитка уже открыта
+            element.classList.remove("revealed");
+            element.classList.add("not-revealed"); // Переворачиваем ее обратно
+        }
     });
-}
+});
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
+let nbCoups = 0;
 
 const mainDiv = document.createElement('div') as HTMLDivElement;
     mainDiv.setAttribute("id", "mainDiv")
@@ -7,47 +8,52 @@ const mainDiv = document.createElement('div') as HTMLDivElement;
     mainDiv.style.display = "flex"
     mainDiv.style.backgroundColor = "gray"
     mainDiv.style.border = "1px solid black"
+
 const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
 btnStart.addEventListener("click", () => {
     init()
 });
-const container = document.querySelector('#container') as HTMLDivElement;
-    container.style.margin = "0 0 20px 0";
-
-const card_front = document.createElement('div') as HTMLDivElement;
-    card_front.setAttribute("class", "card_front");
-
-
 
 let nb_jeu = 0;
+
+
 function init() {
     console.log('init');
     btnStart.remove();
+    nbCoups = 0;
     nb_jeu++;
     container.innerHTML = `
         <button id="init-button">Nouveau Click</button>
         <p>nombre de tour:${nb_jeu}</p>
+        <p>nombre de tour:${nbCoups}</p>
      
     `;
     
     container.appendChild(mainDiv);
+
     // Create new tiles for this round
     const tiles = new Array(16).fill('').map((_, i) => {
         const tile = document.createElement("div");
-        tile.setAttribute("class", "tile");
+        tile.setAttribute("class", "tile not-revealed");
+        tile.setAttribute("color", colors[Math.floor(i / 2)])
         tile.style.width = "50px";
         tile.style.height = "50px";
         tile.style.border = "1px solid black";
         tile.style.margin = "20px";
-        tile.style.backgroundColor = "black";
+        tile.classList.add(colors[Math.floor(i / 2)]);
         return tile;
+
     });
+
     // Shuffle the new tiles
     tiles.sort(() => Math.random() - 0.5);
+    
     // Clear previous tiles in mainDiv
     mainDiv.innerHTML = '';
+    
     // Add the new tiles to mainDiv
     tiles.forEach(tile => mainDiv.appendChild(tile));
+    
     // Add an event listener
     const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
     newBtnStart.addEventListener("click", () => {
@@ -57,19 +63,41 @@ function init() {
 
     let nodeList = document.querySelectorAll(".tile");
     let elements = Array.from(nodeList);
-    elements.forEach( (element, i) => {
-        element.setAttribute("class", "not-revealed")
+
+    let carreChoisi = null;
+    let propCol = null;
+
+    let revealed = true;
+    
+    elements.forEach((element, i) => {
         element.addEventListener("click", () => {
-            console.log(element.style.backgroundColor)
+            nbCoups++;
             if (element.classList.contains("not-revealed")) {
-                element.style.backgroundColor = "black"; // Change background color to black
                 element.classList.remove("not-revealed");
-                element.classList.add("revealed");
+                if (!carreChoisi) {
+                    carreChoisi = element;
+                    propCol = carreChoisi.getAttribute("color")
+                } else {
+                    if(propCol === element.getAttribute("color") ) {
+                        element.classList.add("revealed")
+                        carreChoisi = null;
+                        propCol = null;
+                    } else {
+                        setTimeout(() => {
+                            carreChoisi.classList.add("not-revealed");
+                            element.classList.add("not-revealed");
+                            carreChoisi = null;
+                            propCol = null;
+                        }, 500);
+                    }
+                }
             } else {
-                element.style.backgroundColor = colors[Math.floor(i / 2)]; // Change background color back to original color
-                element.classList.remove("revealed");
-                element.classList.add("not-revealed");
+                console.log("eeee")
             }
-        })
-    })
+        });
+        
+    });
+
+  
+
 }
